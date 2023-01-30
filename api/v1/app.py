a3528023ea9677195270d319e78c1175b533eff5
David Surumen <davidsurumen@gmail.com>
2023-01-30 20:40:52 +0300

    task 5

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """The API module for the project's part 3"""
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -15,6 +15,12 @@ app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 def do_teardown(exception):
     """Calls close() for this app's context"""
     storage.close()
+
+
+@app.errorhandler(404)
+def error(e):
+    """handler for 404 errors"""
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
