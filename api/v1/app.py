875d17eed90d95947283beb4e6b582a4fae86c6c
David Surumen <davidsurumen@gmail.com>
2023-01-30 18:49:06 +0300

    task 3

---
+++ api/v1/app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""The API module for the project's part 3"""
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def do_teardown():
+    """Calls close() for this app's context"""
+    storage.close()
+
+
+
+if __name__ == "__main__":
+    host = getenv('HBNB_API_HOST')
+    port = getenv('HBNB_API_PORT')
+
+    if host and port:
+        app.run(host=host, port=port, threaded=True)
+    else:
+        app.run(host='0.0.0.0', port='5000', threaded=True)
