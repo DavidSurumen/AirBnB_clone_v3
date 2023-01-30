6f154bd17e4c2cc65822edd9e977e2658aee1c40
David Surumen <davidsurumen@gmail.com>
2023-01-30 18:59:46 +0300

    task 3

---
+++ api/v1/views/index.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""index module"""
+from api.v1.views import app_views
+from flask import jsonify
+
+@app_views.route('/status')
+def route(self):
+    return jsonify('{"status": "OK"}')
