0e8d62763eed507ebdb595d22ab106b2100a7547
David Surumen <davidsurumen@gmail.com>
2023-02-05 02:30:53 +0300

    .

--- models/place.py
+++ models/place.py
@@ -23,8 +23,12 @@ class Place(BaseModel, Base):
     """Representation of Place """
     if models.storage_t == 'db':
         __tablename__ = 'places'
-        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        city_id = Column(String(60), ForeignKey('cities.id',
+                                                ondelete='CASCADE'),
+                         nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id',
+                                                ondelete='CASCADE'),
+                         nullable=False)
         name = Column(String(128), nullable=False)
         description = Column(String(1024), nullable=True)
         number_rooms = Column(Integer, nullable=False, default=0)
@@ -33,10 +37,12 @@ class Place(BaseModel, Base):
         price_by_night = Column(Integer, nullable=False, default=0)
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
+
         reviews = relationship("Review", backref="place")
         amenities = relationship("Amenity", secondary="place_amenity",
                                  backref="place_amenities",
                                  viewonly=False)
+        __mapper_args__ = {"confirm_deleted_rows": False}
     else:
         city_id = ""
         user_id = ""
